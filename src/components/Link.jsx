283d22032936e88d443094277bc27e14353897da
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-21 14:29:36 +0200

    finished props

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,8 +1,13 @@
-export function Link() {
-    const address = "https://zrozumiecreact.pl";
+const address = "https://zrozumiecreact.pl";
+export function Link ({text = address, shouldOpenNewTab}) {
+     console.log(text);
+    const address = "https//:zrozumiecreact.pl";
+    const target = shouldOpenNewTab ? "_blank" : "";
+    const rel = shouldOpenNewTab ? "noopener noreferer" : ""
+ 
     return (
-        <a href={address} target="_blank" rel="noopener noreferrer">
-            Sprawdź stronę teraz!
+        <a href={address} target={target} rel={rel}>
+            {text}
         </a>
     );
 }
