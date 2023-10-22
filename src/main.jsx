3c2bca5356dbef71c4fceef983df59fa0c1f4bf3
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-22 11:01:57 +0200

    init conditionalRendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,4 @@
-import ReactDOM from "react-dom/client";
-import { Link } from "./components/Link";
-import { HeadingItalic } from "./components/HeadingItalic";
-import { Logger } from "./components/Logger";
-const element = (
-    <>
-        <Logger>
-        <HeadingItalic><Link /></HeadingItalic>
-        </Logger>
-        <main>
-            <Link text="sprawdź stronę kursu!" shouldOpenNewTab={true} />
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
-                labore blanditiis magnam eveniet rem ratione, voluptatum nulla
-                inventore quaerat necessitatibus.
-            </p>
-            <Logger>
-            <Link  shouldOpenNewTab={false}/>
-            </Logger>
-        </main>
-    </>
-);
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
