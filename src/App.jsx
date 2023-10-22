0bb15dc44bc7d57e2c2aa54203c7848e98c659f8
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-22 17:22:12 +0200

    finished conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 
+import { PersonInfo } from "./components/PersonInfo";
 function App() {
+  const jan = {
+    name: "Jan",
+    lastName: "Kowalski",
+    mail: "j.kowalski@gmail.com",
+    tel: "+48 999 999 999"
+  };
+  const janina = {
+    name: "Janina",
+    lastName: "Kowalska",
+    mail: "j.kowalska@gmail.com",
+    tel: "+48 999 999 888"
+  };
+  const karol = {
+    name: "Karol",
+    lastName: "Kowalski",
+    mail: "k.kowalski@gmail.com",
+    
+
+  };
   return (
-    <div>test</div>
-  )
+    <>
+    <PersonInfo person={jan} />
+    <br />
+   <PersonInfo person={janina} />
+   <br />
+   <PersonInfo person={karol} />
+   </>
+   
+
+  );
 }
 
 export default App;
