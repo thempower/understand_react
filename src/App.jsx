403c13cbbe37aba8fbdbd8f835093e0ef439d09c
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-24 23:28:21 +0200

    finished events

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-
 function App() {
-
-  
+  function handleClick(){
+   { alert("klik!!") }
+  }
+  return (
+    <>
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+       <h2>Fabuła</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quae!</p>
+      <button
+        onClick={(handleClick)}>
+       pokaz spoiler</button>
+      <p>Vader okazuje sie byc ojcem luka</p>
+      
+    </>
+  )
 }
 
 export default App;
+//done//
