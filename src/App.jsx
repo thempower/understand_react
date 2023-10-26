e158f1662b8f032f7dea44a9a4abfaf3b5cf683d
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-26 22:49:24 +0200

    finished useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
 function App() {
-  function handleClick(){
-   { alert("klik!!") }
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isButtonShown, setIsButtonShown] = useState(true);
+    const [isWarningShown, setIsWarningShown] = useState(true);
+  console.log(isSpoilerShown);
+
+  function handleShowSpoilerClick() {
+    setIsSpoilerShown(true);
+    setIsButtonShown(false);
+    setIsWarningShown(false);
+    console.log("handle click");
+        //close warning
   }
+
+  function handleCloseWarningClick(){
+    //close warning
+    setIsWarningShown(false);
+    console.log("close warning");
+  }
+
+  console.log("APP RENDER");
   return (
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
-       <h2>Fabuła</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quae!</p>
-      <button
-        onClick={(handleClick)}>
-       pokaz spoiler</button>
-      <p>Vader okazuje sie byc ojcem luka</p>
-      
+      <h2>Fabuła</h2>
+      {isWarningShown && (
+      <p>Uwaga! Opis fabuły zawiera spoilery!<button onClick={handleCloseWarningClick}>X</button></p>
+      )}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quae!
+      </p>
+      {isButtonShown && <button onClick={handleShowSpoilerClick}>pokaz spoiler</button>}
+      {isSpoilerShown && <p> Vader okazuje sie byc ojcem luka</p>}
     </>
-  )
+  );
 }
 
 export default App;
