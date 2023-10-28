64f7df906150f0bbce37e0deb0a66518edd651e8
[Krzysztof Staszewski desktop] <krzysiek.staszewski@gmail.com>
2023-10-28 22:49:18 +0200

    finished stateUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 function App() {
   const [isSpoilerShown, setIsSpoilerShown] = useState(false);
   const [isButtonShown, setIsButtonShown] = useState(true);
-    const [isWarningShown, setIsWarningShown] = useState(true);
+  const [isWarningShown, setIsWarningShown] = useState(true);
+  const [numberOfLikes, setnumberOfLikes] = useState(50);
   console.log(isSpoilerShown);
 
   function handleShowSpoilerClick() {
@@ -19,11 +20,24 @@ function App() {
     console.log("close warning");
   }
 
+  function handleLikeButtonClick(){
+    setnumberOfLikes(numberOfLikes + 1);
+
+  }
+
+  function handleLoveButtonClick(){
+    setnumberOfLikes(numberOfLikes + 1);
+
+  }
+
   console.log("APP RENDER");
   return (
     <>
       <h1>Gwiezdne wojny V</h1>
       <h2>Rok produkcji: 1980</h2>
+      <h2>Liczka polubień: {numberOfLikes}</h2>
+      <button onClick={handleLikeButtonClick}>Lubię TO!</button>
+      <button onClick={handleLoveButtonClick}>Kocham to!</button>
       <h2>Fabuła</h2>
       {isWarningShown && (
       <p>Uwaga! Opis fabuły zawiera spoilery!<button onClick={handleCloseWarningClick}>X</button></p>
